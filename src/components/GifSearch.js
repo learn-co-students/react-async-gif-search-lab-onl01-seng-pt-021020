9a67f3ed06b8f9c8add76e323f87e422ff8e6875
jkatem <jenmiao@gmail.com>
2020-11-02 20:22:24 -0800

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,15 +1,30 @@
-import React from 'react'
 
 // this component will render a form that receives users input
 // user input should be a controlled component that stores the value of the input 
 
+import React, { Component } from 'react'
+
 class GifSearch extends Component {
 
-    render() {
-        return (
-            console.log("I will be a form")
-        )
-    }
+  state = {
+    query: ""
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.fetchGIFs(this.state.query)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.query} onChange={event => this.setState({query: event.target.value})} />
+        </form>
+      </div>
+    )
+  }
+
 }
 
-export default GifSearch 
+export default GifSearch
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -3,32 +3,35 @@
 // and passing that data down to its child, the <Giflist> component, as a prop
 
 import React, { Component } from 'react'
-import GifList from './components/GifList'
-import GifSearch from './components/GifSearch'
-
-
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
 
 class GifListContainer extends Component {
-    state = {
-        gifs: []
-    }
 
-    componentDidMount() {
-        fetch('https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=YOUR API KEY&rating=g')
-        .then(resp => resp.json)
-        .then(data => {
-            this.setState({
-                gifs: data.map ( gif => ({ url: gif.images.original.url}))
-            })
-        })
-    }
-    
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  state = {
+    gifs: []
+  }
+
+  render() {
+    return(
+      <div>
+        <GifSearch fetchGIFs={this.fetchGIFs} />
+        <GifList gifs={this.state.gifs} />
+      </div>
+    )
+  }
+
+  fetchGIFs = (query = "dolphins") => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+      .then(res => res.json())
+      .then(({data}) => {
+        this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchGIFs()
+  }
 }
+
 export default GifListContainer
