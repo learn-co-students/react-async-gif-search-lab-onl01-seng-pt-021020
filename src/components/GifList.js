9c42636b55fb3ef63f48a64e40622ead91babac8
nicksolony <nicksolony@gmail.com>
2020-11-03 22:08:25 -0500

    added gifs to page

---
+++ src/components/GifList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GifList(props){
+    
+  return (
+    <div>
+      <img src={props.url}/>
+    </div>
+     )
+}
+
+export default GifList
--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import GifList from './GifList'
 
-function GifListContainer(props) {
+class GifListContainer extends React.Component {
 
-    return(
-        <div>container</div>
-    )
+    state={
+        searchResult:[]
+    }
+
+    serchData = (query) =>{
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
+        .then(resp=>resp.json())
+        .then(data=>this.setSearchResults(data.data))
+    }
+
+    setSearchResults = (result) =>{
+        this.setState({
+            searchResult: result.slice(0,3)
+        })
+    }
+
+    render() {
+        this.serchData('cat')
+        return(
+            this.state.searchResult.map(obj=>{
+                return (
+                    <GifList url={obj.images.original.url}/>
+                )
+            })
+            
+        )
+    }
 }
 
 export default GifListContainer
